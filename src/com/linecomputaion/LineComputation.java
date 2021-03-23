cea7a4b226db3d4d0a12a5864597aeb95b868493
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-24 02:09:58 +0530

    checking equality of lengths

--- src/com/linecomputaion/LineComputation.java
+++ src/com/linecomputaion/LineComputation.java
@@ -1,10 +1,18 @@
 package com.linecomputaion;
 import java.util.Scanner;
 public class LineComputation {
+	static boolean equals(double length1,double length2)
+	{
+		if(length1 == length2)
+		{
+			return true;
+		}
+		return false;
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int x1,y1,x2,y2;
-		double dis;
+		System.out.println("enter 1st line co-ordinate");						
 		System.out.println("enter x1 point");
 		x1=sc.nextInt();
 		System.out.println("enter y1 point");
@@ -13,9 +21,22 @@ public class LineComputation {
 		x2=sc.nextInt();
 		System.out.println("enter y2 point");
 		y2=sc.nextInt();
-		dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-
-		System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
+		double disOfLine1=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		int x3,x4,y3,y4;
+		System.out.println("enter 2nd line co-ordinate");						
+		System.out.println("enter x3 point");
+		x3=sc.nextInt();
+		System.out.println("enter y3 point");
+		y3=sc.nextInt();
+		System.out.println("enter x4point");
+		x4=sc.nextInt();
+		System.out.println("enter y4 point");
+		y4=sc.nextInt();
+		double disOfLine2=Math.sqrt((x3-x4)*(x3-x4) + (y3-y4)*(y3-y4));
+		boolean checkEquality =equals(disOfLine1,disOfLine2);
+		if(checkEquality)
+			System.out.println("both lines are equal");
 	}
+		
 }
 
