baf705dce34b0cedc40108ed6b7a56d5aa085fbe
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-24 13:47:36 +0530

    using compareTo()

--- src/com/linecomputaion/LineComputation.java
+++ src/com/linecomputaion/LineComputation.java
@@ -1,14 +1,6 @@
 package com.linecomputaion;
 import java.util.Scanner;
 public class LineComputation {
-	static boolean equals(double length1,double length2)
-	{
-		if(length1 == length2)
-		{
-			return true;
-		}
-		return false;
-	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int x1,y1,x2,y2;
@@ -21,7 +13,7 @@ public class LineComputation {
 		x2=sc.nextInt();
 		System.out.println("enter y2 point");
 		y2=sc.nextInt();
-		double disOfLine1=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		Integer disOfLine1=(int)Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 		int x3,x4,y3,y4;
 		System.out.println("enter 2nd line co-ordinate");						
 		System.out.println("enter x3 point");
@@ -32,11 +24,21 @@ public class LineComputation {
 		x4=sc.nextInt();
 		System.out.println("enter y4 point");
 		y4=sc.nextInt();
-		double disOfLine2=Math.sqrt((x3-x4)*(x3-x4) + (y3-y4)*(y3-y4));
-		boolean checkEquality =equals(disOfLine1,disOfLine2);
-		if(checkEquality)
+		Integer disOfLine2=(int)Math.sqrt((x3-x4)*(x3-x4) + (y3-y4)*(y3-y4));
+		int compResult = disOfLine1.compareTo(disOfLine2);
+		if(compResult > 0)
+		{
+			System.out.println("1st line ig greater than second line");
+		}
+		else if(compResult == 0)
+		{
 			System.out.println("both lines are equal");
+		}
+		else if(compResult < 0)
+		{
+			System.out.println("1st line ig less than second line");
+		}
 	}
-		
+
 }
 
